474e206be01b68f6a7c5368c7af6ea21cf3cf3a1
Pablo Romero <romero7116@gmail.com>
2026-04-17 15:52:53 +0200

    Initial Commit

---
+++ N-Queens/N_QueensApp.swift
@@ -0,0 +1,17 @@
+//
+//  N_QueensApp.swift
+//  N-Queens
+//
+//  Created by Pablo Romero on 17/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct N_QueensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ N-QueensTests/N_QueensTests.swift
@@ -0,0 +1,19 @@
+//
+//  N_QueensTests.swift
+//  N-QueensTests
+//
+//  Created by Pablo Romero on 17/04/2026.
+//
+
+import Testing
+@testable import N_Queens
+
+struct N_QueensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
